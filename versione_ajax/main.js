6cd4acab6e998c2240d1651b52a170bc3e75e99b
FrancescoDale <fd060862@gmail.com>
2020-12-18 16:05:22 +0100

    creazione delle cards e fix del path che provocava la mancata stampa delle cards

--- versione_ajax/main.js
+++ versione_ajax/main.js
@@ -7,12 +7,20 @@ $(document).ready(function() {
         url : 'dischi.php',
         method : 'GET',
         error : function() {
-
+            console.log('error');
         },
         success : function(variable) {
             for (var i = 0; i < variable.length; i++) {
-                console.log(variable[i]);
-            }
+                var objCreator = {
+                    'poster' : variable[i].poster,
+                    'title' : variable[i].title,
+                    'author' : variable[i].author,
+                    'genre' : variable[i].genre,
+                    'year' : variable[i].year,
+                };
+                var html = template(objCreator);
+                $('.cardbox').append(html);
+            };
         },
     });
 });
